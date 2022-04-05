5e857cc2a74c9243ab4da232487a2d1a8ff51884
Tim Crombie <tim.crombie@gmail.com>
2022-04-05 10:46:41 -0500

    update all loggers

--- scripts/process_logger_data.R
+++ scripts/process_logger_data.R
@@ -4,7 +4,7 @@ library(tidyverse)
 setwd(glue::glue("{dirname(rstudioapi::getActiveDocumentContext()$path)}/.."))
 
 # read in button data for each logger
-b1 <- data.table::fread("~/Desktop/20220404_hawaii_temp_button1.csv") %>%
+b1 <- data.table::fread("~/repos/temp_loggers/data/20220404_hawaii_temp_button1.csv") %>%
   tidyr::separate(`Date/Time`, into = c("date", "time", "unit"), sep = " ") %>%
   dplyr::mutate(time2 = lubridate::hms(time),
                 time3 = case_when(unit == "PM" ~ time2+lubridate::hours(12),
@@ -14,11 +14,11 @@ b1 <- data.table::fread("~/Desktop/20220404_hawaii_temp_button1.csv") %>%
                 timezone = "CST",
                 logger = "Oahu",
                 high_alarm = ifelse(Value >= 27, T, F)) %>%
-  dplyr::filter(datetime > lubridate::ymd_hms("2022-4-1 21:00:00")) %>%
+  dplyr::filter(datetime > lubridate::ymd_hms("2022-4-1 21:00:00" & datetime < lubridate::ymd_hms("2022-4-4 12:00:00"))) %>%
   dplyr::select(datetime, timezone, temp_c = Value, logger, high_alarm) %>%
   dplyr::arrange(datetime)
 
-b2 <- data.table::fread("~/Desktop/20220404_hawaii_temp_button2.csv") %>%
+b2 <- data.table::fread("~/repos/temp_loggers/data/20220404_hawaii_temp_button2.csv") %>%
   tidyr::separate(`Date/Time`, into = c("date", "time", "unit"), sep = " ") %>%
   dplyr::mutate(time2 = lubridate::hms(time),
                 time3 = case_when(unit == "PM" ~ time2+lubridate::hours(12),
@@ -28,16 +28,31 @@ b2 <- data.table::fread("~/Desktop/20220404_hawaii_temp_button2.csv") %>%
                 timezone = "CST",
                 logger = "Big Island",
                 high_alarm = ifelse(Value >= 27, T, F)) %>%
-  dplyr::filter(datetime > lubridate::ymd_hms("2022-3-31 17:00:00")) %>%
+  dplyr::filter(datetime > lubridate::ymd_hms("2022-3-31 17:00:00" & datetime < lubridate::ymd_hms("2022-4-4 12:00:00"))) %>%
   dplyr::select(datetime, timezone, temp_c = Value, logger, high_alarm) %>%
   dplyr::arrange(datetime)
-  
+
+b3 <- data.table::fread("~/repos/temp_loggers/data/20220404_hawaii_temp_button3.csv") %>%
+  tidyr::separate(`Date/Time`, into = c("date", "time", "unit"), sep = " ") %>%
+  dplyr::mutate(time2 = lubridate::hms(time),
+                time3 = case_when(unit == "PM" ~ time2+lubridate::hours(12),
+                                  unit != "PM" ~ time2),
+                date2 = lubridate::mdy(date),
+                datetime = lubridate::ymd_hms(date2+time3),
+                timezone = "CST",
+                logger = "Kauai",
+                high_alarm = ifelse(Value >= 27, T, F)) %>%
+  dplyr::filter(datetime > lubridate::ymd_hms("2022-3-28 09:00:00") & datetime < lubridate::ymd_hms("2022-4-4 12:00:00")) %>%
+  dplyr::select(datetime, timezone, temp_c = Value, logger, high_alarm) %>%
+  dplyr::arrange(datetime)
+
 # find high temp alarm run duration in minutes
 alarm_runs1 <- tibble::tibble(run_length = rle(b1$high_alarm)[[1]], value = rle(b1$high_alarm)[[2]], duration = lubridate::minutes((run_length*2))) # yikes!
 alarm_runs2 <- tibble::tibble(run_length = rle(b2$high_alarm)[[1]], value = rle(b2$high_alarm)[[2]], duration = lubridate::minutes((run_length*2)))
+alarm_runs3 <- tibble::tibble(run_length = rle(b3$high_alarm)[[1]], value = rle(b3$high_alarm)[[2]], duration = lubridate::minutes((run_length*2)))
 
 # bind the data for buttons
-bound_df <- dplyr::bind_rows(b1, b2)
+bound_df <- dplyr::bind_rows(b1, b2, b3)
 
 # plot temps with 27C indicator
 p <- ggplot2::ggplot(data = bound_df) +
